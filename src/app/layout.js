98955b10c07b7ac732b63dac2489bf1adf935531
Punnawich Mai <punnawich_dachin@gmail.com>
2023-08-07 14:56:44 +0700

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Punnawich Dach-in"
+            studentId="650610787"
           />
         </div>
       </body>
